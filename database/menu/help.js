52bee6c9c2d722fc6f5166762c038a180e32adbd
MR-NC <61655493+MR-NC@users.noreply.github.com>
2021-01-30 11:27:17 +0700

    Add files via upload

---
+++ database/menu/help.js
@@ -0,0 +1,211 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+「 *${botName}* 」
+
+◪ *USER INFO*
+  ❏ Name: ${pushname}
+  ❏ XP: ${reqXp}
+  ❏ Money: ${uangku}
+  ❏ Registered: ✔️
+◪ *BOT INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+  ❏ Version: 2.0.9
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bug
+◪ *XP*
+  │
+  ├─ ❏ ${prefix}leveling
+  ├─ ❏ ${prefix}level
+  └─ ❏ ${prefix}mining
+◪ *LIMIT*
+  │
+  ├─ ❏ ${prefix}limit
+  ├─ ❏ ${prefix}bal
+  └─ ❏ ${prefix}buylimit
+--------------------------------
+Note:Cek uang kamu dengan cara mengetikan perintah *${prefix}bal*
+
+◪ *IMAGE MAKER*
+  │
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}herrypotter
+  └─ ❏ ${prefix}quotemaker
+◪ *LOGO MAKER*
+
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}jokerlog
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+  
+◪ *MAKER*
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  ├─ ❏ ${prefix}rate
+  └─ ❏ ${prefix}bisakah
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *Search*
+  │
+  ├─ ❏ ${prefix}wait
+  ├─ ❏ ${prefix}ytsearch
+  ├─ ❏ ${prefix}trendtwit
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}wiki
+  └─ ❏ ${prefix}neonime
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+  
+◪ *SOUND*
+  │
+  └─ ❏ ${prefix}tts
+  
+◪ *MUSIC*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ ❏ ${prefix}joox
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+
+◪ *INFORMATION*
+  │
+  ├─ ❏ ${prefix}bahasa
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  ├─ ❏ ${prefix}fakta
+  ├─ ❏ ${prefix}infocuaca
+  ├─ ❏ ${prefix}infogempa
+  ├─ ❏ ${prefix}jadwaltvnow
+  └─ ❏ ${prefix}covidcountry
+◪ *18+*
+  |
+  ├─ ❏ ${prefix}randomhentai
+  ├─ ❏ ${prefix}nsfwtrap
+  └─ ❏ ${prefix}nsfwneko
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}pinterest
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  └─ ❏ ${prefix}tiktok
+◪ *EDUCATION*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  ├─ ❏ ${prefix}tafsirmimpi
+  ├─ ❏ ${prefix}translate
+  └─ ❏ ${prefix}artinama
+◪ *STICKER MAKER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  └─ ❏ ${prefix}ttp
+◪ *TOD*
+  │
+  ├─ ❏ ${prefix}trust
+  └─ ❏ ${prefix}dare
+◪ *FUN*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+◪ *STAY ON SCREEN*
+  │
+  └─ ❏ ${prefix}afk
+
+◪ *GROUP*
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote
+  ├─ ❏ ${prefix}demote
+  ├─ ❏ ${prefix}tagall
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}listadmins
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+  ├─ ❏ ${prefix}nsfw
+  ├─ ❏ ${prefix}delete
+  ├─ ❏ ${prefix}simih
+  ├─ ❏ ${prefix}tagme
+  └─ ❏ ${prefix}ownergroup
+◪ *OWNER*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *OTHER*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2
+  `
+}
+exports.help = help
